b1499f0a83d3b96034111e322233d673bff6c48f
J0onYEong <cjy2134@naver.com>
2023-05-14 14:34:10 +0900

    Initial Commit

---
+++ GongBok/GongBokApp.swift
@@ -0,0 +1,17 @@
+//
+//  GongBokApp.swift
+//  GongBok
+//
+//  Created by 최준영 on 2023/05/14.
+//
+
+import SwiftUI
+
+@main
+struct GongBokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
